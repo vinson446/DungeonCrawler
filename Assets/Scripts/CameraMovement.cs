d4d2472b168fbba6e41f951178268406d378c91c
Benny <liangchengheng1@gmail.com>
2020-05-22 23:54:17 -0500

    Made some calculations for floor tiles and background

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] Transform move;
+
+    private void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal") * 1;
+        move.Translate(moveX,0,0);
+    }
+
+
+}
