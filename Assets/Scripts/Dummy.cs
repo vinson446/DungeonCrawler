a4af2f74ee2bc213bf3c61cb078275143182b1a0
Benny <liangchengheng1@gmail.com>
2020-06-07 01:10:17 -0500

    added dummy w/ animation and a slashing effect press 'X' when you approach the dummy

---
+++ Assets/Scripts/Dummy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+    [SerializeField] GameObject slash;
+    Animator animator;
+    bool damaged;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        damaged = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isDamaged();
+
+    }
+
+    void isDamaged()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            animator.Play("Dummy_Damaged");
+            slash.SetActive(true);
+        }
+    }
+
+}
